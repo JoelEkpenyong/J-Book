b28b5a066db20622acef3817baf15ef47e5a18b1
JoelEkpenyong <joelekpenyong@gmail.com>
2022-06-11 21:43:29 +0100

    reset the iframe on submit

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ const App = () => {
 
   const onClick = async () => {
     if (!ref.current) return;
+
+    iFrame.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -39,7 +42,13 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', event => {
-            eval(event.data);
+            try {
+              eval(event.data);
+            } catch (err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"> <h4>Runtime Error</h4>' + err + '</div>'
+              throw err
+            }
           }, false);
         </script>
       </body>
